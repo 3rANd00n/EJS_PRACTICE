6fd1b286dfd6a7b8eb2c0249d5d2e2bd6316f9ae
3rANd00n <quz22fau@uea.ac.uk>
2024-03-26 16:03:41 +0000

    controllers

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+
+const tasks = require('./routes/tasks')
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to my Task Manager Project');
 });
+
+app.use('/api/v1/tasks', tasks);
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}...`);
